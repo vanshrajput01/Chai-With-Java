d6dd250b777b3981dd5aee4c222863e89a71e3d0
Vansh Rajput <rajputvansh009@example.com>
2024-05-21 17:44:03 +0530

    Primitive data types in java

--- Core_Java_concepts/DataTypes.java
+++ Core_Java_concepts/DataTypes.java
@@ -1,6 +1,26 @@
 public class DataTypes{
     public static void main(String[] args){
         System.out.println("Data Types in Java!!");
+        System.out.println("Primitive data types in Java!!");
+
+        int number1 = 100;
+
+        short number2 = 10;
+
+        long number3 = 10000;
+
+        char char01 = 'A';
+
+        boolean bool1 = false;
+
+        float decimalValue01 = 23.04f;
+
+        double decimalValue02 = 2345.0768;
+
         
+
+        
+
+
     }
 }
