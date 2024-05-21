0a869b1e2ba00260f968a6c01f79777be04fcaeb
Vansh Rajput <rajputvansh009@example.com>
2024-05-21 17:57:04 +0530

    final commit in datatype in java

--- Core_Java_concepts/DataTypes.java
+++ Core_Java_concepts/DataTypes.java
@@ -19,17 +19,17 @@ public class DataTypes{
 
         System.out.println("int data type in java " + number1);
 
-        System.out.println("short data type in java" + number2);
+        System.out.println("short data type in java " + number2);
 
         System.out.println("Long interger data type in java " + number3);
 
         System.out.println("Float data type in java " + decimalValue01);
 
-        System.out.println("Double float data type in java" + decimalValue02);
+        System.out.println("Double float data type in java " + decimalValue02);
 
-        System.out.println("char data type in java" + char01);
+        System.out.println("char data type in java " + char01);
 
-        System.out.println("Boolean data type in java" + bool1);
+        System.out.println("Boolean data type in java " + bool1);
 
 
 
