9292a7983b10b7b991aa87345dec85574a8addb4
Vansh Rajput <rajputvansh009@example.com>
2024-05-21 17:33:40 +0530

    example2 of variable in java

--- Core_Java_concepts/Program2OfVariable.java
+++ Core_Java_concepts/Program2OfVariable.java
@@ -1,6 +1,8 @@
 public class Program2OfVariable{
     public static void main(String[] args) {
         System.out.print("Example 02in Variable in java!!");
+        int result = 10 + 20;       
+        System.out.println(result);
         
     }
 }
