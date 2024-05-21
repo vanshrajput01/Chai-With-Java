6625728d018fd67418650869cf5c5f9953700023
Vansh Rajput <rajputvansh009@example.com>
2024-05-21 17:49:30 +0530

    Primitive data types in java

--- Core_Java_concepts/DataTypes.java
+++ Core_Java_concepts/DataTypes.java
@@ -17,7 +17,24 @@ public class DataTypes{
 
         double decimalValue02 = 2345.0768;
 
-        
+        System.out.println("int data type in java " + number1);
+
+        System.out.println("short data type in java" + number2);
+
+        System.out.println("Long interger data type in java " + number3);
+
+        System.out.println("Float data type in java " + decimalValue01);
+
+        System.out.println("Double float data type in java" + decimalValue02);
+
+        System.out.println("char data type in java" + char01);
+
+        System.out.println("Boolean data type in java" + bool1);
+
+
+
+
+
 
         
 
