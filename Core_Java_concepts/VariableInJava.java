e4795737bbf7a60343bb5799868fc7978dd37d02
Vansh Rajput <rajputvansh009@example.com>
2024-05-21 17:24:40 +0530

    Variable in Java 'Frist Progarm'

---
+++ Core_Java_concepts/VariableInJava.java
@@ -0,0 +1,11 @@
+public class VariableInJava{
+    public static void main(String[] args){
+        System.out.println("Variable in Java!!");
+        // Syntax of declare variable in Java
+        // Datatype variable_name = value;
+        int num1 = 10;
+        int num2 = 12;
+        int results = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + results);
+    }
+}
