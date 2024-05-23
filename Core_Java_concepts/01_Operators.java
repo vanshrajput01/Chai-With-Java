95898d74f9a29bfd339e38ce0465b2dcccf64e52
Vansh Rajput <rajputvansh009@example.com>
2024-05-23 13:30:43 +0530

    Unary operators in java

--- Core_Java_concepts/01_Operators.java
+++ Core_Java_concepts/01_Operators.java
@@ -1,5 +1,6 @@
-import java.awt.SystemTray;
+
 import java.util.Scanner;
+
 
 public class FirstsProgram{
     public static void main(String[] args){
@@ -16,5 +17,19 @@ public class FirstsProgram{
         System.out.println(num1 / num2);
         System.out.println(num1 % num2);
 
+        // Unary operator in java
+        System.out.println("Unary oparator in java");
+        System.out.println(num1++);// like value is 10 that give output 10 but after printing that increasing value of num1
+        System.out.println(num1--); // like value is 11 that that give me output 11 but after printing that decreasing value of num1
+
+        System.out.println(++num2); //like value of num2 is 2 that give me value 3 beacause this increase/decrese the value firstly and print it. 
+        System.out.println(--num2);
+
+    
+
+
+
     }
+
+
 }
