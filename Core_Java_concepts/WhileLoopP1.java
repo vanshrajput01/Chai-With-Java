8e77170e0930b58ce89abe9746a9a8ddfdc3c447
Vansh Rajput <rajputvansh009@example.com>
2024-05-24 11:41:53 +0530

    While Loop in java

--- Core_Java_concepts/WhileLoopP1.java
+++ Core_Java_concepts/WhileLoopP1.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
 public class WhileLoopP1{
-    public static main void(String[] args){
-        
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Sum of numbers!!");
+        int sumOfNumbers = 0;
+        while(true){
+            System.out.println("Please enter any number :- !! if You see output of your program then pass value is -1 ");
+            int numberValue = s.nextInt();
+            if (numberValue  < 0){
+                System.out.println("Output your program is :- ");
+                System.out.println(sumOfNumbers);
+                break;
+
+                
+
+            }else{
+                sumOfNumbers = sumOfNumbers + numberValue;
+            }
+
+
+        }
+
     }
 }
