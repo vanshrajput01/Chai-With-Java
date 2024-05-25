d683b8516c40c0938cc0d7980ed6d236af8397a9
Vansh Rajput <rajputvansh009@example.com>
2024-05-25 11:09:29 +0530

    continue statement in java

--- Core_Java_concepts/ContinueStatementInJava.java
+++ Core_Java_concepts/ContinueStatementInJava.java
@@ -1,2 +1,18 @@
 // Continue Statement in Java
 
+public class ContinueStatementInJava{
+    public static void main(String[] args){
+        System.out.println("Print 1 to 50 but not print multiple 5");
+        for(int i = 0;i<=50;i++){
+            if (i % 5 == 0) {
+                continue;
+                
+            }
+            System.out.println(i);
+
+        }
+
+
+
+    }
+}
