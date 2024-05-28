c838ec7b462b2df1494e7ce2330ef34ea86be7bf
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-28 22:12:15 +0530

    method in java problem 02 injava

---
+++ Core_Java_concepts/OOP_In_Java/MethodInJavaP2.java
@@ -0,0 +1,24 @@
+// Java Program to add two numbers using methods
+
+
+public class Main{
+    // create add method
+    static int add(int a,int b){
+        int result;
+        result = a + b;
+        return result;
+
+    } 
+
+
+    public static void main(String[] args){
+        int ans =  add(2, 3);
+        System.out.println(ans);
+
+
+    }
+
+}
+
+
+
