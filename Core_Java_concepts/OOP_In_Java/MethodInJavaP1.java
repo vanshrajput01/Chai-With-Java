308f703925c418268c011827dd15a39cf57c2469
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-28 22:08:12 +0530

    method in java problem 01

---
+++ Core_Java_concepts/OOP_In_Java/MethodInJavaP1.java
@@ -0,0 +1,18 @@
+// Java Program to create method.
+
+
+public class Main{
+    // create welcome method 
+    static void welcome(){
+        System.out.println("Welcome to my repo!!");
+
+    }
+    public static void main(String[] args){
+        welcome(); // call the method
+        welcome();
+
+
+    }
+
+}
+
