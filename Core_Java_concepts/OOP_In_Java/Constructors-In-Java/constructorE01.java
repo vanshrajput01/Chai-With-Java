3a3e40730f56856154c5e307e78ca924aa3b2624
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-30 10:54:58 +0530

    defual constructor example 01 in java

---
+++ Core_Java_concepts/OOP_In_Java/Constructors-In-Java/constructorE01.java
@@ -0,0 +1,22 @@
+// example of defual constructor in java
+
+// class Algebra{
+//     void printMessage(){
+//         System.out.println("This is the method of Algebra class and method name is print message!!");
+//     } 
+
+
+
+// }
+
+public class constructorE01{
+    void printMessage(){
+        System.out.println("this is print method!!");
+    }
+    public static void main(String[] args) {
+        constructorE01 objE01 = new constructorE01(); // here call the defaul constructor in java
+        objE01.printMessage();
+
+       
+    }
+}
