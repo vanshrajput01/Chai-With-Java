b513cf3df2b9cae5c3f50752180c07b68b469eba
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-05-31 16:38:35 +0530

    example of Traversing Element Of Array in java

---
+++ Core_Java_concepts/Data-Type_In_Java/Arrays-In-Java/TraversingElementOfArray.java
@@ -0,0 +1,17 @@
+// Traversing element of an array in java 
+
+public class P1ArraysInJava{
+    static int numbersArray[] = {1,2,3,4,5,6,10,7,8};
+    public static void main(String[] args) {
+        // For each loop in java
+        for(int ele : numbersArray){
+            System.out.println(ele);
+        }
+
+
+
+
+
+        
+    }
+}
