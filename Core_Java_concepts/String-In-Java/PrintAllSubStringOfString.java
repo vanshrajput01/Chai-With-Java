8dda47bbf6e8676e006a65a6c0a2c3fd02db8226
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-02 10:33:57 +0530

    Print all Substring Of String.java

---
+++ Core_Java_concepts/String-In-Java/PrintAllSubStringOfString.java
@@ -0,0 +1,25 @@
+// print all substrig of string in java 
+
+public class StringEx01{
+
+    public static void main(String[] args) {
+            //like string   
+            String str = "Hello";
+            for (int i = 0; i < str.length(); i++) {
+                for (int j = i+1; j <= str.length(); j++) {
+                    System.out.print(str.substring(i,j));
+                    System.out.print(",");
+                    
+
+
+
+                    
+                }
+                
+            }
+
+
+      
+    }
+}
+
