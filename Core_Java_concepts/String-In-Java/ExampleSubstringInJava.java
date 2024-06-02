529b49949ccbd7c159999390df4f356a85285769
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-02 10:13:38 +0530

    Example Substring In Java

---
+++ Core_Java_concepts/String-In-Java/ExampleSubstringInJava.java
@@ -0,0 +1,15 @@
+// Example of substrig in java 
+
+public class StringEx01{
+
+    public static void main(String[] args) {
+            String str = "Hello";
+            // method first
+            System.out.println("Substring method  : " + str.substring(2));
+            // method second 
+            System.out.println("Substring method  : " + str.substring(0,3));
+
+      
+    }
+}
+
