e2f30c4bdd811e5b01eebca489cd177e08da1093
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-02 09:43:31 +0530

    example 01 of string

---
+++ Core_Java_concepts/String-In-Java/StringEx01.java
@@ -0,0 +1,10 @@
+// Example 01 based on Strig in java
+
+public class StringEx01{
+
+    public static void main(String[] args) {
+            String str = "Hello";
+            System.out.println("Sring in java :- " + str);
+        
+    }
+}
