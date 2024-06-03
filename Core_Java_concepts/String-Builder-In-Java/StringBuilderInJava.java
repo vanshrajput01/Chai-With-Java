b4bf20ea3dd37206f6a618c8fd9519fc8d34bfe9
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-03 12:54:06 +0530

    String Builder In Java

---
+++ Core_Java_concepts/String-Builder-In-Java/StringBuilderInJava.java
@@ -0,0 +1,12 @@
+// Example of string builder in java
+public class StringBuilderInJava{
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Hello");
+        System.out.println(str);
+        // String builder methods in java
+        
+
+    }
+
+    
+}
