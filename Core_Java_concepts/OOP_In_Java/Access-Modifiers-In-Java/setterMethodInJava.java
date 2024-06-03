ee0a1aaa48eff09502291bc44a9ac8958c2c3748
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-03 21:26:14 +0530

    setter method in java

---
+++ Core_Java_concepts/OOP_In_Java/Access-Modifiers-In-Java/setterMethodInJava.java
@@ -0,0 +1,45 @@
+// getter and setter method in java
+
+class Company{
+    int empId;
+    String empName;
+    // private Access modifer in java
+    private int empSalary;
+
+    // getter method in java
+
+    public void getSalary(){
+        System.out.println(empSalary);
+    } 
+
+    // setter method in java
+    public void setSalary(int sal){
+        empSalary = sal;
+
+    }
+    
+
+}
+
+
+public class GetterAndSetterMethodInJava{
+    public static void main(String[] args) {
+        Company emp01 = new Company();
+        emp01.empId = 101;
+        emp01.empName = "Mark Wood";
+        System.out.println("employee details is :- " + emp01.empName + " " + emp01.empId);
+        // emp01.empSalary = 70000; Error because this is private variable not accss outside of calss
+        // using gette method to access private attributes of comapny class
+        emp01.getSalary(); 
+        // usring setter method to set value of private attribute 
+        emp01.setSalary(60000); // set salary of employee;
+        emp01.getSalary();   // get salary of employee;
+
+
+        
+
+    
+    }
+
+
+}
