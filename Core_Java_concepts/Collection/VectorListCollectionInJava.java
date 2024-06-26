7b56d6ecf35b3e87fd558f703028daa57825ca14
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-26 12:05:23 +0530

    Traversing vector in list collection using method of list collection

--- Core_Java_concepts/Collection/VectorListCollectionInJava.java
+++ Core_Java_concepts/Collection/VectorListCollectionInJava.java
@@ -14,11 +14,34 @@ public class ArrayCollectionInJava{
         animalNames.add("Dogs");
         animalNames.add("deer");
 
-        // Traversing Vector using foreach methid in java
+        // Traversing Vector using foreach method in java
 
         animalNames.forEach(ele->{
             System.out.print(ele);
         });
+
+        // Traversing Vector using iterable method in java
+
+        for(String aniamlName : animalNames){
+            System.out.println(aniamlName);
+        }
+
+        // Traversing Vector using iterator method in java
+
+        Iterator  objIterator = animalNames.iterator();
+        while(objIterator.hasNext()){
+            System.out.println(objIterator.next());
+
+        }
+
+        // Traversing vector backward direction using ListIterator method in java
+
+        ListIterator objListIterator = animalNames.listIterator(animalNames.size());
+
+        while(objListIterator.hasPrevious()){
+            System.out.print(objListIterator.previous());
+
+        }
 
         
 
