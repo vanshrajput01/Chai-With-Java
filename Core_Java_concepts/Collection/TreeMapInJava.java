6a9ca43cffc50f53dbde050166fcbda7dbec8f86
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-26 15:47:20 +0530

    Example of tree Map in Sorted Map collection in java

---
+++ Core_Java_concepts/Collection/TreeMapInJava.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+// Hash Map in java
+
+public class MapCollectionInJava{
+    public static void main(String[] args) {
+
+
+        // Example of tree Map in SortedMap collection in java
+        TreeMap <Integer,Integer> numberTreeMap = new TreeMap<>();
+
+        numberTreeMap.put(1, 101);
+        numberTreeMap.put(12, 178);
+        numberTreeMap.put(3, 161);
+        numberTreeMap.put(6, 105);
+        numberTreeMap.put(5, 102);
+
+        System.out.println(numberTreeMap);
+
+
+        
+
+
+
+
+        
+
+
+       
+
+
+
+
+    
+
+        
+
+    }
+}
