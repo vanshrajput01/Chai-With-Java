9e1ba571cd73bd32942b46bb134d0488523436dc
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-26 11:51:53 +0530

    example of Vector List collection in java

---
+++ Core_Java_concepts/Collection/VectorListCollectionInJava.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+// Vector collection in Java  
+
+
+
+public class ArrayCollectionInJava{
+    public static void main(String[] args) {
+
+        Vector<String> animalNames = new Vector<>();
+
+        animalNames.add("Cats");
+        animalNames.add("Dogs");
+        animalNames.add("deer");
+
+        
+
+ }
+}
