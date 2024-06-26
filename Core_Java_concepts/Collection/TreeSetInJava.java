8777555f012591ab57ae45ecc2c6af5a31dd833a
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-06-26 12:58:46 +0530

    Tree set in java

---
+++ Core_Java_concepts/Collection/TreeSetInJava.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class SetCollectionInJava{
+    public static void main(String[] args) {
+
+        // In TreeSet collection duplicate are not allowed 
+        HashSet<Integer> numberValues = new HashSet<>();
+
+        numberValues.add(112);
+        numberValues.add(101);
+        numberValues.add(102);
+        numberValues.add(103);
+        numberValues.add(104);
+        numberValues.add(101);
+        numberValues.add(102);
+        numberValues.add(103);
+        numberValues.add(106);
+
+        TreeSet<Integer> numberValues01 = new TreeSet<>();
+
+        numberValues01.addAll(numberValues);
+
+        System.out.println(numberValues01);
+
+
+        
+
+
+
+
+
+        
+
+
+
+
+        
+    }
+}
